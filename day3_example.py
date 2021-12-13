eef4124e6d773cf6125abd021e83afa3b6e8b548
Azarberg <95284599+Azarberg@users.noreply.github.com>
2021-12-13 10:43:23 +0600

    d3

---
+++ day3_example.py
@@ -0,0 +1,120 @@
+#Проблема 2:
+a=2**3
+b=3**2
+print(a)
+print(b)
+if a>b:
+    print(a)
+elif a<b:
+    print(b)
+else:
+    print(erorr)
+#Проблема 22:
+a=12
+b=6
+c=5
+
+if  b>a and b>c:
+    print('b=6')
+elif a>b and  a>c:
+    print('a=12')
+else:
+    print('c=5')
+
+if b<a and  b<c:
+    print('b=6')
+elif a<b and a<c:
+    print('a=12')
+else:
+    print('c=5')
+#Проблема 15:
+a=17391%17
+b=546%17
+c=934%17
+
+if b<a and  b<c:
+    print(b, "b")
+elif a<b and a<c:
+    print(a,'a')
+else:
+    print(c,'c')
+
+#Проблема 39:
+x=13
+
+if x**2>172:
+     print('x  в квадрате больше 172')
+elif x**2**2>172:
+     print('x  в квадрате  возведенное еще раз квадрат больше 172')
+
+
+#Проблема 5:
+a=50
+
+if a%2==0:
+     print('a - четное')
+elif a%2!=0:
+     print('a -нечетное')
+if a%3==0:
+     print('a - делится на 3 без остатка')
+elif a%3!=0:
+     print('a -не делится на 3 без остатка')
+if a**2>1000:
+     print('a в квадрате больше 1000')
+elif a**2<1000:
+     print('a в квадрате меньше 1000')
+
+#Проблема 3:
+
+a=2
+
+if a>=0 and  a<22 or a>56 and a<101:
+    print('a  входит в разрешенный диапазон')
+elif a<0 and a>21 and a>100:
+    print('a  не входит в разрешенный диапазон')
+
+   
+#Проблема 7:
+if 2>1:
+    print('true')
+#Проблема 9:
+a=1000
+if a>500:
+    print('by phone')
+    if a>600:
+        print('by  phone watch')
+        if a>900:
+            print('by ipod')
+
+#Проблема 45:
+a=10//5
+b=10/5
+if a==b:
+    print('a=b')
+    if a==b:
+        print(a+b)
+elif a!=b:
+    print('a!=b')
+
+#Проблема 33:
+
+a=-5
+
+if a<0:
+    print ('a-negative number')
+elif a>0:
+    print ('a-positive number')
+
+#Проблема 11:
+a=10
+b=5
+if a>=0 and b>=0:
+    print('a and b  positive number')
+#Проблема 0:
+a=10
+b=5
+if a>b:
+    print(a+2,'a+2')
+else:
+    print(b+2,'b+2')
+
