d68d556a4643130e104429cb0d4cbc81155e74c6
Azarberg <95284599+Azarberg@users.noreply.github.com>
2021-12-13 10:43:02 +0600

    day6

---
+++ set.py
@@ -0,0 +1,22 @@
+#Проблема 0
+i = {6, 21, 18}
+i.remove(6)
+print(i)
+
+#Проблема 2
+farm_1 = {'dog','cat','mouse','sheep'}
+farm_2 = {'cow','horse','donkey','cat','dog'}
+s = farm_2.difference(farm_1)
+print(s)
+#Проблема 3
+s = { 1,'f',4,'gggg',9}
+s.add('i')
+s.pop() #  Удаляет рандомный обьект
+print(s)
+#Проблема 1
+s = {3,1}
+p =  {4}
+l = {'aaaa'}
+
+print(s.union(p,l))
+
